3d2ed4c4ccf0fedb8ededa225fd51cc2a6ec263b
Thomas Peuss <thomas@peuss.de>
2014-02-14 20:50:44 +0100

    Let LED on pin 12 blink as long as we have no GPS lock. Steady light when we have a lock.

--- examples/gpsserial.ino
+++ examples/gpsserial.ino
@@ -1,23 +1,3 @@
-/*
-Arduino application that talks to Celestron Nexstar compatible mounts like the original Celestron GPS.
-
-Copyright 2014 Thomas Peuss <thomas at peuss dot de>
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
- 
 #include <TinyGPS.h>
 #include <NexstarMount.h>
 #include <SoftwareSerial.h>
@@ -25,6 +5,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define RX_PIN 3
 #define TX_PIN 4
 #define LED_PIN 13
+
+#define SIGNAL_PIN 12
 
 #define RX2_PIN 7
 #define TX2_PIN 8
@@ -40,9 +22,21 @@ TinyGPS gps;
 NexstarMessageReceiver msg_receiver;
 NexstarMessageSender msg_sender(&gps,RTS_PIN,CTS_PIN);
 
+int ledState = LOW;             // ledState used to set the LED
+long previousMillis = 0;        // will store last time LED was updated
+
+// the follow variables is a long because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long interval = 1000;           // interval at which to blink (milliseconds)
+
+boolean haveLock=false;
+
 void setup() {
   pinMode(LED_PIN,OUTPUT);
   digitalWrite(LED_PIN,LOW);
+  
+  pinMode(SIGNAL_PIN,OUTPUT);
+  digitalWrite(SIGNAL_PIN,LOW);
   
   mountserial.begin(19200);
   
@@ -87,10 +81,31 @@ void loop() {
   
     gps.get_datetime(NULL,NULL,&fix_age);
     
-    if (fix_age==gps.GPS_INVALID_AGE || fix_age>5000) {
-      digitalWrite(LED_PIN,LOW);
+    if (fix_age==gps.GPS_INVALID_AGE || fix_age>5000 || gps.satellites()==gps.GPS_INVALID_SATELLITES || gps.satellites()<4) {
+      haveLock=false;
     } else {
-      digitalWrite(LED_PIN,HIGH);
+      haveLock=true;
+    }
+  }
+
+  if (haveLock) {
+      digitalWrite(LED_PIN, HIGH);
+      digitalWrite(SIGNAL_PIN, HIGH);         
+  } else {
+    unsigned long currentMillis = millis();  
+    if(currentMillis - previousMillis > interval) {
+      // save the last time you blinked the LED 
+      previousMillis = currentMillis;   
+  
+      // if the LED is off turn it on and vice-versa:
+      if (ledState == LOW)
+        ledState = HIGH;
+      else
+        ledState = LOW;
+  
+      // set the LED with the ledState of the variable:
+      digitalWrite(LED_PIN, ledState);
+      digitalWrite(SIGNAL_PIN, ledState);
     }
   }
 }
